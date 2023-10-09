b3787a34a97546e1c696b7ef3976156621bcce76
minhal15 <64689808+minhal15@users.noreply.github.com>
2023-10-09 14:59:31 -0400

    Added Login event handlers

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import { Link , useNavigate } from "react-router-dom";
 
 const Login = () => {
+  // state variables
+  const [firstName, setFirstName] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleFirstNameChange = (event) => {
-
+  // Event handlers
+  const handleEmailChange = (event) => {
+    setFirstName(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
-   
+    setPassword(event.target.value);
   };
 
   const handleSubmit = (event) => {
+    event.preventDefault();
 
+    // Local storage is not working, try something else
+    const storedFirstName = localStorage.getItem("firstName");
+    const storedPassword = localStorage.getItem("password");
+
+    if (storedFirstName === firstName && storedPassword === password) {
+      navigate("/content");
+    } else {
+      alert("Invalid credentials. Please try again.");
+    }
   };
   
+
   return (
     <>
       <div className="h-screen w-screen bg-[#F0E7DC]">
         <Navbar />
         <div className="flex justify-center items-center mt-10 h-5/6">
           <div className="w-5/12 h-5/6  px-2 py-2 ">
-            <h3 className="font-semibold text-sm text-[#349795]">Login</h3>
+            <h3 className="font-semibold text-sm text-[#349795]">LOGIN</h3>
             <h2 className="font-bold text-xl mt-2 ">
               Simplify Contact Management
             </h2>
@@ -31,13 +46,17 @@ const Login = () => {
         <input
           className="bg-[#F0E7DC] mt-6 py-1.5 w-5/6 border-b-2 border-black"
           type="text"
-          placeholder="First Name"
+          placeholder="Email"
+          value={firstName}
+          onChange={handleEmailChange}
         />
 
         <input
           className="bg-[#F0E7DC] mt-4 py-1.5 w-5/6 border-b-2 border-black"
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
         />
         <button className="w-5/6 bg-[#349795] rounded mt-8 py-1.5 text-white" type="submit">
           Login
